dbf9c0eb59c4df35c552238ced1ead0dd05f0eda
senorcl0ud <claudemcalpin@gmail.com>
2018-07-12 23:22:12 -0400

    fixed endpoint mapping and added import for person-detail component

--- Person-API-Angular/src/app/app.module.ts
+++ Person-API-Angular/src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import { PersonDetailComponent } from './person-detail/person-detail.component';
 
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'people', pathMatch: 'full' },
-  { path: 'person/:id', component: PersonDetail },
-  { path: 'person', component: PersonComponent }
+  { path: '', redirectTo: '/people', pathMatch: 'full' },
+  { path: '/person/:id', component: PersonDetailComponent },
+  { path: '/person', component: PersonComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonComponent
+    PersonComponent,
+    PersonDetailComponent
   ],
   imports: [
     FormsModule,
