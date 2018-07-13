ffa7116ccb0ae208d032b5a202a05ab240f5da08
imowbray97 <isaiah.mowbray97@gmail.com>
2018-07-13 15:08:23 -0400

    Update person component

--- Person-API-Angular/src/app/person/person.component.ts
+++ Person-API-Angular/src/app/person/person.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Person } from '../person';
 import { PersonService } from '../service/person.service';
 import {Routes} from "@angular/router";
+import {Location} from "@angular/common";
 
 
 @Component({
@@ -11,18 +12,28 @@ import {Routes} from "@angular/router";
 })
 export class PersonComponent implements OnInit {
 
-  person = Person;
-
-  constructor(private personService: PersonService) { }
+  person = new Person();
+  summit = false;
+  constructor(private personService: PersonService, private  location:Location) { }
 
   ngOnInit() {
-    this.create(this.person);
+
   }
 
-  create(person: any): any {
-    this.personService.create(person)
+  create(): any {
+    this.personService.create(this.person)
       .subscribe(person => this.person = person);
   }
 
+
+
+  onSubmit() {
+    this. summit = true;
+    this.create()
+  }
+
+  goBack() {
+    this.location.back();
+  }
 
 }
